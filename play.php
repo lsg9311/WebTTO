8c0b378832584e274fc179cc1e88d08d3ffb69c3
MOVABLEWORK\ouiqdmw <ouiqdmw17@gmail.com>
2016-12-03 11:37:21 +0900

    PNG file for PLAY TOP uploaded

--- play.php
+++ play.php
@@ -10,3 +10,6 @@
 	Your browser does not support the HTML5 canvas tag.
 	</canvas>
 </div>
+<img src="TOP_CLIENTNAME.png" id="NAME_IMG" width="32" height="64">
+<img src="TOP_ClIENTSLOT.png" id="SLOT_IMG" width="32" height="64">
+<img src="TOP_MAP.png" id="MAP_IMG" width="32" height="64">
