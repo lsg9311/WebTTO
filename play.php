b34c706ec7deaf6839bf278455db36526ad33098
MOVABLEWORK\ouiqdmw <ouiqdmw17@gmail.com>
2016-12-03 11:22:09 +0900

    PLAY main uploaded

---
+++ play.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="play.css">
+<div id="PLAY-TOP">
+	<canvas width="1500" height="100" style="border:1px solid #000000;">
+	Your browser does not support the HTML5 canvas tag.
+	</canvas>
+</div>
+<div id="PLAY-MAIN">
+	<canvas width="1500" height="600" style="border:1px solid #000000;">
+	Your browser does not support the HTML5 canvas tag.
+	</canvas>
+</div>
