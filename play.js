9ae8ffae1a3ec03967f45f2cdda40329c4b86c38
lsg9311 <crit9411@naver.com>
2016-12-03 13:22:32 +0900

    scroll background

--- play.js
+++ play.js
@@ -1,12 +1,36 @@
+//img Option
 var backgroundIMG = new Image();
 backgroundIMG.src="image/back_cave.png";
 
-var mainCTX = document.getElementById("PLAY-MAIN").
+//canvas Option
+var canvasHeight=600;
+var canvasWidth=1500;
+var mainCanvas,mainCtx;
+
+//scroll Option
+var scrollVal=0;
+var speed=50;
+var interval_speed=100;
+
+function initCanvas(){
+	mainCanvas = document.getElementById("MAIN-CANVAS");
+	mainCtx = mainCanvas.getContext("2d");
+}
 
 function update_bg(){
+	mainCtx.clearRect(0,0,canvasWidth,canvasHeight);
 
+    scrollVal+=speed;                   
+    mainCtx.drawImage(backgroundIMG,2000-scrollVal,0,2000,canvasHeight);
+    mainCtx.drawImage(backgroundIMG,scrollVal,0,2000,2000,0, 0, 2000,canvasHeight);
+
+
+    if(scrollVal >= 2000){
+        scrollVal = 0;
+    }
 };
 
-window.onload=function(){
-
-};
+$(document).ready(function(){
+	initCanvas();
+	var intervalID=setInterval(update_bg,interval_speed);
+});
--- play.php
+++ play.php
@@ -1,16 +1,23 @@
+<html>
+<head>
 <link rel="stylesheet" type="text/css" href="play.css">
 <script src="lib/jquery-3.1.1.min.js"></script>
-<script src="play.js"></script>
+<script src="lib/jcanvas.min.js"></script>
+<script src="./play.js"></script>
+</head>
+<body>
 <div id="PLAY-TOP">
-	<canvas width="1500" height="100" style="border:1px solid #000000;">
+	<canvas id="TOP-CANVAS" width="1500" height="100" style="border:1px solid #000000;">
 	Your browser does not support the HTML5 canvas tag.
 	</canvas>
 </div>
 <div id="PLAY-MAIN">
-	<canvas width="1500" height="600" style="border:1px solid #000000;">
+	<canvas id="MAIN-CANVAS" width="1500" height="600" style="border:1px solid #000000;">
 	Your browser does not support the HTML5 canvas tag.
 	</canvas>
 </div>
+</body>
 <img src="TOP_CLIENTNAME.png" id="NAME_IMG" width="32" height="64">
 <img src="TOP_ClIENTSLOT.png" id="SLOT_IMG" width="32" height="64">
 <img src="TOP_MAP.png" id="MAP_IMG" width="32" height="64">
+</html>
