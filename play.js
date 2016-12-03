32edaa6cd9510bc45915a39a98e1c6262566da9e
MOVABLEWORK\ouiqdmw <ouiqdmw17@gmail.com>
2016-12-03 11:53:01 +0900

    image location modified image declaration move to js file

--- play.js
+++ play.js
@@ -1,6 +1,13 @@
 var backgroundIMG = new Image();
 backgroundIMG.src="image/back_cave.png";
 
+var TOP_NAME_IMG = new Image();
+TOP_NAME_IMG = "image/TOP_CLIENTNAME.png";
+var TOP_SLOT_IMG = new Image();
+TOP_SLOT_IMG = "image/TOP_ClIENTSLOT.png";
+var TOP_MAP_IMG = new Image();
+TOP_MAP_IMG = "image/TOP_MAP.png";
+
 var mainCTX = document.getElementById("PLAY-MAIN").
 
 function update_bg(){
