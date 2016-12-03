360a7c1ac5cc154c6936fdd9b501962559edf64f
lsg9311 <crit9411@naver.com>
2016-12-03 13:57:32 +0900

    rearrange

--- play.js
+++ play.js
@@ -1,6 +1,13 @@
 //img Option
 var backgroundIMG = new Image();
 backgroundIMG.src="image/back_cave.png";
+var TOP_NAME_IMG = new Image();
+TOP_NAME_IMG = "image/TOP_CLIENTNAME.png";
+var TOP_SLOT_IMG = new Image();
+TOP_SLOT_IMG = "image/TOP_ClIENTSLOT.png";
+var TOP_MAP_IMG = new Image();
+TOP_MAP_IMG = "image/TOP_MAP.png";
+
 
 //canvas Option
 var canvasHeight=600;
@@ -16,15 +23,6 @@ function initCanvas(){
 	mainCanvas = document.getElementById("MAIN-CANVAS");
 	mainCtx = mainCanvas.getContext("2d");
 }
-
-var TOP_NAME_IMG = new Image();
-TOP_NAME_IMG = "image/TOP_CLIENTNAME.png";
-var TOP_SLOT_IMG = new Image();
-TOP_SLOT_IMG = "image/TOP_ClIENTSLOT.png";
-var TOP_MAP_IMG = new Image();
-TOP_MAP_IMG = "image/TOP_MAP.png";
-
-var mainCTX = document.getElementById("PLAY-MAIN").
 
 function update_bg(){
 	mainCtx.clearRect(0,0,canvasWidth,canvasHeight);
