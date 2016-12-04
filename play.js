5171e2e0a030a0456f3e0151cc45b12d6ee79428
lsg9311 <crit9411@naver.com>
2016-12-04 11:23:40 +0900

    fix sk

--- play.js
+++ play.js
@@ -85,10 +85,12 @@ function update_bg(){
 };
 
 function scroll_bg(){
-	scrollVal+=speed;
 	if(scrollVal >= 2000) scrollVal = 0;
 	if(Math.abs(scrollWall) >=50) Walldir*=-1;
-	scrollWall+=Walldir*speed;   
+	if(GAME_STATE!=0){
+		scrollVal+=speed;
+		scrollWall+=Walldir*speed;
+	}   
 	if(scrollVal >= 2000){
 		scrollVal = 0;
 	}
@@ -108,7 +110,7 @@ function draw_bg(){
 	//draw bg
     ctxBuffer.drawImage(backgroundIMG,2000-scrollVal,0,2000,canvasHeight);
     ctxBuffer.drawImage(backgroundIMG,scrollVal,0,2000,2000,0, 0, 2000,canvasHeight);
-    draw_wall(scrollVal,scrollWall);
+    draw_wall(ctxBuffer,scrollVal,scrollWall);
 
     //draw character
     if(frame1<RPM+1){ctxBuffer.drawImage(bird1, cx, cy, 50, 50);}
@@ -136,16 +138,16 @@ function flying(){
 
 
 //render wall img
-function draw_wall(scroll,scrollWall){
-	mainCtx.save();
+function draw_wall(ctx,scroll,scrollWall){
+	ctx.save();
 	var wall = mainCtx.createPattern(wallIMG,"repeat");
-	mainCtx.fillStyle=wall;
+	ctx.fillStyle=wall;
 	
-	var scrollCoef=scroll*2;
+	var scrollCoef=scroll*4;
 
-	mainCtx.translate(scrollCoef,0);
+	ctx.translate(scrollCoef,0);
     // draw
-    mainCtx.fillRect(-scrollCoef, 0, 1500, 100);
+    ctx.fillRect(-scrollCoef, 0, 1500, 100);
 /*
     
     mainCtx.moveTo(-scrollCoef,100);
@@ -156,7 +158,7 @@ function draw_wall(scroll,scrollWall){
 
     mainCtx.fill();
     mainCtx.fillStyle=wall;
-*/    mainCtx.fillRect(-scrollCoef, 500, 1500, 100);
+*/    ctx.fillRect(-scrollCoef, 500, 1500, 100);
 /*    mainCtx.moveTo(-scrollCoef,500);
     mainCtx.lineTo(-scrollCoef,500-scrollWall)
     mainCtx.bezierCurveTo(1000-(scrollCoef),400+scrollWall,1000-(scrollCoef),400+scrollWall,1500-(scrollCoef),400+scrollWall);
@@ -167,8 +169,8 @@ function draw_wall(scroll,scrollWall){
     mainCtx.fillStyle=wall;
 */
     // undo offset
-    mainCtx.translate(-scrollCoef, 0);
-    mainCtx.restore();
+    ctx.translate(-scrollCoef, 0);
+    ctx.restore();
 };
 
 // update top canvas
@@ -308,7 +310,7 @@ var death_time;
 //ready state show
 function ready_canvas(){
 	update_top(CLIENT_SLOT, CLIENT_NAME, CLIENT_SIZE, HPLEFT, HPMAX, global_time_tick, death_time);	// FOR TESTING PURPOSE
-	update_bg();
+	draw_bg();
 
 	test();
 }
@@ -356,7 +358,6 @@ function update_all() {
 		case 0:
 			ready_canvas();
 			ready_indicate();
-			console.log(indicate_time);
 			if(indicate_time==0) GAME_STATE=1;
 		break;
 		// on running status
