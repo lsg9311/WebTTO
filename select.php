622239978390d84d59f818c83ce4c1d95eb29a8d
blytheej@naver.com <blytheej@naver.com>
2016-12-04 11:52:10 +0900

    add bird image

--- select.php
+++ select.php
@@ -12,11 +12,31 @@
 			color:white;
 			font-size: 23px;
 		}
+		
+		img{
+			width : 180px;
+			height : 150px;
+		}
 
 	</style>
 </head>
 <body bgcolor = "#A9D0F5">
 	
+	<div> 
+		<img src="image/bird/blue/blue.gif" id="blue">
+	</div>
+	<div>
+		<img src="image/bird/blue/blue.gif" id="blue">
+	</div>
+	<div>
+		<img src="image/bird/blue/blue.gif" id="blue">
+		<img src="image/bird/blue/blue.gif" id="blue">
+		<img src="image/bird/blue/blue.gif" id="blue">
+		<img src="image/bird/blue/blue.gif" id="blue">
+
+	</div>
+
+
 	<div style="clear:both; margin:auto; width:350px; height:75px;">
 		<input id="exit" type="button" value="나가기"/>
 	</div>
