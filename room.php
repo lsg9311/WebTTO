e56f14bd97e6bcced98289a15d71d6f851496c15
blytheej@naver.com <blytheej@naver.com>
2016-12-04 16:17:22 +0900

    room.php

--- room.php
+++ room.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html>
 <head>
+	<script src="lib/jquery-3.1.1.min.js"></script>
 	<style>
+
 		#user_container{
 			margin-left: 130px;
 			margin-top: 40px;
@@ -110,22 +112,22 @@
 	<div id="entire">
 	<div id ="user_container" style="background-color:'#2E64FE';">
 		<div>
-			<img src="" id="user1" width="120px" height="120px">
-			<img src="" id="user2" width="120px" height="120px">
-			<img src="" id="user3" width="120px" height="120px">
+			<img id="user1" width="120px" height="120px">
+			<img src="image/bird/chicken/chicken.gif" id="user2" width="120px" height="120px">
+			<img src="image/bird/dragon/dragon.gif" id="user3" width="120px" height="120px">
 		</div>
 		<div>
-			<img src="" id="user4" width="120px" height="120px">
-			<img src="" id="user5" width="120px" height="120px">
-			<img src="" id="user6" width="120px" height="120px">
+			<img src="image/bird/duck/duck.gif" id="user4" width="120px" height="120px">
+			<img src="image/bird/monster/monster.gif" id="user5" width="120px" height="120px">
+			<img src="image/bird/pink/pink.gif" id="user6" width="120px" height="120px">
 		</div>
 	</div>
 	
 	<div id ="ready_container">
 		<select id="map_select" name="Map Select">
-			<option value="map1">이학관별관</option>
+			<option value="map1">601호</option>
 			<option value="map2">정보관</option>
-			<option value="map3">미융관</option>
+			<option value="map3">닥터fish</option>
 		</select>
 		<img id="map_image" src="" width="170px" height="170px">
 		<div>
@@ -145,6 +147,31 @@
 			</div>
 		</div>
 	</div>
+	<script>
+	var wsUri;
+	var character;
 
+	function select(character){
+		var imglink = "image/bird/"+character+"/"+character+".gif";
+		return imglink;
+	}
+	// get the picture
+	$(document).ready(function(){
+
+		wsUri = "ws://localhost:9000/kweb/mc/server.php"; 	
+		websocket = new WebSocket(wsUri); 
+
+		websocket.onmessage = function(ev) {
+			var msg = JSON.parse(ev.data);
+			var type = msg.type;
+			if(type == 'charsel'){
+				character = msg.charater;
+				
+			}
+			$("#user1").src = select(character);
+		}
+	});
+
+	</script>
 </body>
 </html>
