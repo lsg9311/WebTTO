5b246130f46e336e4189ce5a2e0f48eac6f3ecdc
blytheej@naver.com <blytheej@naver.com>
2016-12-04 11:19:40 +0900

    image

---
+++ select.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<style>
+		
+		
+		#exit{
+			background-color: RoyalBlue;
+			margin-top: 25px;
+			width:80%;
+			height:50px;
+			color:white;
+			font-size: 23px;
+		}
+
+	</style>
+</head>
+<body bgcolor = "#A9D0F5">
+	
+	<div style="clear:both; margin:auto; width:350px; height:75px;">
+		<input id="exit" type="button" value="나가기"/>
+	</div>
+</body>
+</html>
