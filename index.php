5f8d837d1809d3800eb8e24d27f8a2da4be90948
lsg9311 <crit9411@naver.com>
2016-12-04 13:16:05 +0900

    index page setting

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="index.css"/>
+<link rel="stylesheet" type="text/css" href="login.css"/>
+
 <script src="lib/jquery-3.1.1.min.js"></script>
 <script src="lib/jcanvas.min.js"></script>
-<script src="./play.js"></script>
+<script src="./index.js"></script>
 </head>
 <body>
 </body>
--- login.php
+++ login.php
@@ -1,63 +1,4 @@
-<html>
-<head>
-<style>
-	#head_container{
-		margin-top: 120px;
-		width : 200px;
-		height: 70px;
-		margin-left: auto;
-		margin-right: auto;
-		text-align: center;
-	}
-	
-	#img_star{
-		vertical-align: middle;
-		float: left;
-	}
-
-	#submit{
-		width: 300px;
-		clear: both;
-		margin : auto;
-		margin-top: 70px;
-	}
-
-	#nickname{
-		font-size: 15px;
-		vertical-align: middle;
-		width:300px;
-		height: 35px;
-		padding-top: 3px;
-		padding-left: 5px;
-	}
-
-	#input_enter{
-		width:100px;
-		height: 38px;
-		float:left;
-		font-size: 16px;
-	}
-
-	#input_grade{
-		width:100px;
-		height: 38px;
-		float:right;
-		font-size: 16px;
-	}
-
-	#input_container{
-		margin-top: 40px;
-	}
-
-	h1{
-		vertical-align: middle;
-		font-size: 45px;
-	}
-
-</style>
-
-</head>
-<body bgcolor = "#A9D0F5">
+<div id="login">
 	<div id = "head_container">
 		<img src="image/logo.png" id="img_star" width="80px" height="80px">
 		<h1>웹또</h1>
@@ -75,6 +16,4 @@
 			</div>
 		</div>
 	</form>
-
-</body>
-</html>
+</div>
