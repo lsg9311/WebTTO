8e1e64bf5d3b9753af7c9fb2798eb600c0b34c11
lsg9311 <crit9411@naver.com>
2016-12-04 13:00:20 +0900

    make index

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<link rel="stylesheet" type="text/css" href="play.css">
+<html>
+<head>
 <script src="lib/jquery-3.1.1.min.js"></script>
 <script src="lib/jcanvas.min.js"></script>
 <script src="./play.js"></script>
-
-<div id="PLAY-TOP">
-	<canvas id="TOP-CANVAS" width="1500" height="100" style="border:1px solid #000000;">
-	Your browser does not support the HTML5 canvas tag.
-	</canvas>
-</div>
-<div id="PLAY-MAIN">
-	<canvas id="MAIN-CANVAS" width="1500" height="600" style="border:1px solid #000000;">
-	Your browser does not support the HTML5 canvas tag.
-	</canvas>
-</div>
-
+</head>
+<body>
+</body>
+</html>
