1466f6c0164d481bcf7104aeb419edf3dbf98896
lsg9311 <crit9411@naver.com>
2016-12-04 14:28:14 +0900

    stateful page

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+/*Page State
+0 : login.php
+1 : lobby.php
+2 : room.php
+3 : game.php
+4 : result.php
+*/
+var STATE = 3;
+
 $(document).ready(function(){
-	$("body").load("login.php");
+	state_change();
+	$("#input_enter").on("click",function(){
+		STATE=1;
+		state_change();
+	});
 });
+
+function state_change(){
+	switch(STATE){
+		case 0:
+			$("body").load("login.php");
+		break;
+		case 1:
+			$("body").load("lobby.php");
+		break;
+		case 2:
+			$("body").load("room.php");
+		break;
+		case 3:
+			$("body").load("play.php");
+		break;
+		case 4:
+			$("body").load("result.php");
+		break;
+	}
+}
